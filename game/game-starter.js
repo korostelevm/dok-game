81685de43e220bfa5c2da9608a18a1038ee10a47
Vincent Le Quang <vinceq@amazon.com>
2021-12-19 13:57:01 -0800

    ccc

--- game/game-starter.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-	// Engine.start(Platformer, "games/sample-platformer/sample-platformer.json");
-	Engine.start(GameTitle);
-});
--- game/lib/utils/string.js
+++
@@ -1,7 +0,0 @@
-class StringUtil {
-    static kebabToClass(input) {
-        return input.replace(/-?\b([a-z])/g, function (g) {
-            return g[g.length - 1].toUpperCase();
-        });
-    }	
-}
