adc68281f28f8b8a18804a9406fffa6867949672
Jack Le Hamster <jacklehamster@gmail.com>
2022-02-07 23:28:35 -0800

    Fix opengl on mobile.

--- data/fragmentShader.glsl
+++ data/fragmentShader.glsl
@@ -13,10 +13,10 @@ in float v_light;
 
 out vec4 outColor;
 
-vec4 getTextureColor(sampler2D textures[NUM_TEXTURES], float textureSlot, vec2 vTexturePoint);
+vec4 getTextureColor(float textureSlot, vec2 vTexturePoint);
 
 void main() {
-	vec4 color = getTextureColor(uTextures, v_index, v_textureCoord);
+	vec4 color = getTextureColor(v_index, v_textureCoord);
 	if (color.a <= 0.01) {
 		discard;
 	}
@@ -25,54 +25,54 @@ void main() {
 }
 
 const float threshold = 0.00001;
-vec4 getTextureColor(sampler2D textures[NUM_TEXTURES], float textureSlot, vec2 vTexturePoint) {
+vec4 getTextureColor(float textureSlot, vec2 vTexturePoint) {
 	if (abs(0.0 - textureSlot) < threshold) {
-		return texture(textures[0], vTexturePoint);
+		return texture(uTextures[0], vTexturePoint);
 	}
 	if (abs(1.0 - textureSlot) < threshold) {
-		return texture(textures[1], vTexturePoint);
+		return texture(uTextures[1], vTexturePoint);
 	}
 	if (abs(2.0 - textureSlot) < threshold) {
-		return texture(textures[2], vTexturePoint);
+		return texture(uTextures[2], vTexturePoint);
 	}
 	if (abs(3.0 - textureSlot) < threshold) {
-		return texture(textures[3], vTexturePoint);
+		return texture(uTextures[3], vTexturePoint);
 	}
 	if (abs(4.0 - textureSlot) < threshold) {
-		return texture(textures[4], vTexturePoint);
+		return texture(uTextures[4], vTexturePoint);
 	}
 	if (abs(5.0 - textureSlot) < threshold) {
-		return texture(textures[5], vTexturePoint);
+		return texture(uTextures[5], vTexturePoint);
 	}
 	if (abs(6.0 - textureSlot) < threshold) {
-		return texture(textures[6], vTexturePoint);
+		return texture(uTextures[6], vTexturePoint);
 	}
 	if (abs(7.0 - textureSlot) < threshold) {
-		return texture(textures[7], vTexturePoint);
+		return texture(uTextures[7], vTexturePoint);
 	}
 	if (abs(8.0 - textureSlot) < threshold) {
-		return texture(textures[8], vTexturePoint);
+		return texture(uTextures[8], vTexturePoint);
 	}
 	if (abs(9.0 - textureSlot) < threshold) {
-		return texture(textures[9], vTexturePoint);
+		return texture(uTextures[9], vTexturePoint);
 	}
 	if (abs(10.0 - textureSlot) < threshold) {
-		return texture(textures[10], vTexturePoint);
+		return texture(uTextures[10], vTexturePoint);
 	}
 	if (abs(11.0 - textureSlot) < threshold) {
-		return texture(textures[11], vTexturePoint);
+		return texture(uTextures[11], vTexturePoint);
 	}
 	if (abs(12.0 - textureSlot) < threshold) {
-		return texture(textures[12], vTexturePoint);
+		return texture(uTextures[12], vTexturePoint);
 	}
 	if (abs(13.0 - textureSlot) < threshold) {
-		return texture(textures[13], vTexturePoint);
+		return texture(uTextures[13], vTexturePoint);
 	}
 	if (abs(14.0 - textureSlot) < threshold) {
-		return texture(textures[14], vTexturePoint);
+		return texture(uTextures[14], vTexturePoint);
 	}
 	if (abs(15.0 - textureSlot) < threshold) {
-		return texture(textures[15], vTexturePoint);
+		return texture(uTextures[15], vTexturePoint);
 	}
-	return texture(textures[0], vTexturePoint);
+	return texture(uTextures[0], vTexturePoint);
 }
